14211d7f240cb343ce25704829fe0cccd5c0aa1a
alifeyzi <110728669+alifeyzi01@users.noreply.github.com>
2024-09-23 08:02:04 -0700

    Delete coding-project/login-form directory

--- coding-project/login-form/js/js-style.js
+++
@@ -1,5 +0,0 @@
-function login(){
-    var name = document.getElementById("name").value
-    var family = document.getElementById("family").value
-    alert(name + " " + family + " " +  "عزیز ثبت نام شما با موفقیت انجام شد.")
-}
