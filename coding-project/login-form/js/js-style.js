f077227d0c0a866fc6ba19547d1650368a5bdaab
Ali Feyzi <alifeyzi0082gmail.com>
2024-09-23 07:59:51 -0700

    commit two project

---
+++ coding-project/login-form/js/js-style.js
@@ -0,0 +1,5 @@
+function login(){
+    var name = document.getElementById("name").value
+    var family = document.getElementById("family").value
+    alert(name + " " + family + " " +  "عزیز ثبت نام شما با موفقیت انجام شد.")
+}
